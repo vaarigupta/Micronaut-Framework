dc84363939fe754d3df765194a091560d470e034
Vaari.Gupta <vaari.gupta@precisely.com>
2023-12-27 02:18:20 +0530

    Logging implemented

--- demo/demo/src/main/java/com/example/Hello/HelloController.java
+++ demo/demo/src/main/java/com/example/Hello/HelloController.java
@@ -4,11 +4,14 @@ package com.example.Hello;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Controller("/hello")
 public class HelloController {
 
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
     private final HelloServiceInterface service;
 
     public HelloController(HelloServiceInterface service) {
@@ -19,6 +22,8 @@ public class HelloController {
     @Get(produces = MediaType.TEXT_PLAIN)
     public String HelloWorld()
     {
+
+        LOG.debug("calling from Hello World API");
         return service.HelloFromService();
     }
 }
