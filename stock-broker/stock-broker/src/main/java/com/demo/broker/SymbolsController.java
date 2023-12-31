e01f2192f7432dd4df0e3678a22142b264dab37a
Vaari.Gupta <vaari.gupta@precisely.com>
2024-01-01 03:00:40 +0530

    Created endpoint with  query parameters

--- stock-broker/stock-broker/src/main/java/com/demo/broker/SymbolsController.java
+++ stock-broker/stock-broker/src/main/java/com/demo/broker/SymbolsController.java
@@ -4,9 +4,11 @@ import com.demo.broker.data.InMemoryStore;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller("/symbols")
 public class SymbolsController {
@@ -27,5 +29,14 @@ public class SymbolsController {
     public Symbol GetSymbolByValue(@PathVariable String value)
     {
         return inMemoryStore.getSymbols().get(value);
+    }
+
+    @Get("filter{?max}")
+    public List<Symbol> GetSymbolsByQuery(@QueryValue Optional<Integer> max)
+    {
+        return inMemoryStore.getSymbols().values()
+                .stream()
+                .limit(max.orElse(0))
+                .toList();
     }
 }
